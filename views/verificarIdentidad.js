e77032fdbe8428a7ceadca5437ee1cdd68d18358
unknown <Administrator@PRUEBAENEXT.LOCAL>
2025-01-28 12:07:30 -0500

    cambios

--- views/verificarIdentidad.js
+++ views/verificarIdentidad.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
 import GoIdentitySVG from "../public/img/goIdentity.svg";
 import InstitutoSVG from "../public/img/instituto.svg";
 import UsuarioSvg from "../public/img/usuarios.jpeg";
-import LogoEjercito from "../public/img/ejercito.png"
+import LogoEjercito from "../public/img/ejercito.png";
+import { RNCamera } from 'react-native-camera';
 import menuEstilos from '../public/css/menu';
 
 export default function VerificarIdentidad() {
+    const [scanned, setScanned] = useState(false);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        alert(`Código de barra ${type} con datos ${data} ha sido escaneado!`);
+    };
+
     return (
         <LinearGradient
             colors={['#bed9f4', '#c4f4fd', '#ecf2ff', "white"]}
@@ -17,10 +25,21 @@ export default function VerificarIdentidad() {
             end={{ x: 0, y: 1 }}
             style={menuEstilos.container}
         >
+            {/* QR Code Scanner */}
+            <RNCamera
+                style={styles.camera}
+                onBarCodeRead={scanned ? undefined : handleBarCodeScanned}
+                type={RNCamera.Constants.Type.back}
+            />
+            {scanned && (
+                <TouchableOpacity onPress={() => setScanned(false)} style={styles.button}>
+                    <Text style={styles.buttonText}>Escanear nuevamente</Text>
+                </TouchableOpacity>
+            )}
+
             {/* Header */}
             <View style={menuEstilos.header}>
                 <GoIdentitySVG style={menuEstilos.logo} />
-
             </View>
 
             {/* Title */}
@@ -32,12 +51,12 @@ export default function VerificarIdentidad() {
                     <Text style={menuEstilos.cardTitle}>ISSFA</Text>
                 </View>
                 <View style={menuEstilos.cardContent}>
-                    <View >
-                        <Image source={UsuarioSvg}></Image>
+                    <View>
+                        <Image source={UsuarioSvg} />
                     </View>
                     <View style={menuEstilos.cardInfo}>
                         <View style={menuEstilos.imagenCard}>
-                            <Image source={LogoEjercito} style={menuEstilos.logosEjercito}></Image>
+                            <Image source={LogoEjercito} style={menuEstilos.logosEjercito} />
                         </View>
                         <View style={menuEstilos.subida}>
                             <Text style={menuEstilos.cardText}>CÉDULA:</Text>
@@ -57,3 +76,23 @@ export default function VerificarIdentidad() {
         </LinearGradient>
     );
 }
+
+const styles = StyleSheet.create({
+    camera: {
+        height: 200,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#007AFF',
+        padding: 10,
+        borderRadius: 5,
+        marginVertical: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+});
