3299c77dc823f9017850f1afc7cf18cf3ec7ca9e
Robin-Rivadeneira <rivadeneirarobin2@gmail.com>
2025-03-15 22:03:39 -0500

    Cambios

--- src/services/coneccion.Service.js
+++ src/services/coneccion.Service.js
@@ -1,6 +1,6 @@
 const apiConeccion = {
-    coneccionGeneral:  'http://52.70.109.55:3006/api',
-    coneccionRegsitro: 'http://52.70.109.55:3007/api',
+    coneccionGeneral:  'https://52.70.109.55:3006/api',
+    coneccionRegsitro: 'https://52.70.109.55:3007/api',
 }
 
 export default apiConeccion;
